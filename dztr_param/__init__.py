70bd05afc82750df01f5ba4450de2096302e75d3
fuadyassin <fuad.yassin@usask.ca>
2024-07-09 18:59:43 -0600

    Update __init__.py

--- dztr_param/__init__.py
+++ dztr_param/__init__.py
@@ -4,4 +4,4 @@ from .percentiles import calculate_percentiles
 from .data_updater import update_extended_data
 from .csv_creator import create_com_csv
 from .csv_creator_hylakes import create_com_csv_hylakes
-from .txt_creator_hylakes import txt_creator_hylakes
+from .txt_creator_hylakes import create_com_txt_hylakes
